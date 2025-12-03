3a0095f42b3cc5d32baccc7e6d83995f5f100ff2
trngchnii <ni.trnh59@gmail.com>
2025-12-03 23:03:06 +0700

    banner & slidebar UI

--- front-end/shopping-practice/src/pages/users/homePage/index.js
+++ front-end/shopping-practice/src/pages/users/homePage/index.js
@@ -1,6 +1,79 @@
 import { memo } from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import "../homePage/style.scss";
+import "../../../styles/style.scss";
+import item1Img from "../../../asset/users/images/categories/item1.jpg";
+import item2Img from "../../../asset/users/images/categories/item2.jpg";
+import item3Img from "../../../asset/users/images/categories/item3.jpg";
+import item4Img from "../../../asset/users/images/categories/item4.jpg";
+import item5Img from "../../../asset/users/images/categories/item5.jpeg";
+import item6Img from "../../../asset/users/images/categories/item6.jpg";
 const HomePage = () => {
-  return <h1>ABC</h1>;
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  const sliderItems = [
+    {
+      bgImg: item1Img,
+      name: "CRYBABY × Powerpuff Girls Series Figures",
+    },
+    {
+      bgImg: item2Img,
+      name: "SKULLPANDA The Warmth Series",
+    },
+    {
+      bgImg: item3Img,
+      name: "MOLLY × Zootopia Co-branded Series Figures",
+    },
+    {
+      bgImg: item4Img,
+      name: "Celebrating the Moment POP MART 15th Anniversary Series Figures",
+    },
+    {
+      bgImg: item5Img,
+      name: "Babythree v3 mini",
+    },
+    {
+      bgImg: item6Img,
+      name: "Babythree Lucky cat v1",
+    },
+  ];
+
+  return (
+    <>
+      {" "}
+      <div className="container container__categories_slider">
+        <Carousel responsive={responsive} className="categories_slider">
+          {sliderItems.map((item, key) => (
+            <div
+              className="categories_slider_item"
+              style={{ backgroundImage: `url(${item.bgImg})` }}
+              key={key}
+            >
+              <p>{item.name}</p>
+            </div>
+          ))}
+        </Carousel>
+      </div>
+    </>
+  );
 };
 
 export default memo(HomePage);
--- front-end/shopping-practice/src/pages/users/theme/header/index.js
+++ front-end/shopping-practice/src/pages/users/theme/header/index.js
@@ -5,6 +5,7 @@ import {
   AiOutlineInstagram,
   AiOutlineLinkedin,
   AiOutlineMail,
+  AiOutlinePhone,
   AiOutlineShoppingCart,
   AiOutlineUser,
 } from "react-icons/ai";
@@ -90,12 +91,12 @@ const Header = () => {
       </div>
       <div className="container">
         <div className="row">
-          <div className="col-xl-3">
+          <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3">
             <div className="header__logo">
               <h1>Nina Shop</h1>
             </div>
           </div>
-          <div className="col-xl-6">
+          <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6">
             <nav className="header__menu">
               <ul>
                 {menus?.map((menu, menuKey) => (
@@ -115,7 +116,7 @@ const Header = () => {
               </ul>
             </nav>
           </div>
-          <div className="col-xl-3">
+          <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3">
             <div className="header__cart">
               <div className="header__cart__price">
                 <span>{formatter(100)}</span>
@@ -134,8 +135,32 @@ const Header = () => {
         </div>
       </div>
       <div className="container">
-        <div className="row">
-          <div className="col-xl-12 hero__search__container"></div>
+        <div className="row hero__search">
+          <div className="col-xl-9 col-lg-9 col-md-9 col-sm-9 hero_search_form">
+            <form>
+              <input type="text" placeholder="Enter keywords to search…" />
+              <button type="submit">Search</button>
+            </form>
+          </div>
+          <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 hero__search__phone">
+            <div className="hero__search__phone__icon">
+              <AiOutlinePhone />
+            </div>
+            <div className="hero__search__phone__text">
+              <p>070.5355.331</p>
+              <span>Support Available 24/7</span>
+            </div>
+          </div>
+        </div>
+        <div className="hero__item">
+          <div className="hero__text">
+            <span>Blindbox</span>
+            <h2>Babythree - Popmart</h2>
+            <p>Free home delivery</p>
+            <Link to="" className="primary-btn">
+              Buy now
+            </Link>
+          </div>
         </div>
       </div>
     </>
